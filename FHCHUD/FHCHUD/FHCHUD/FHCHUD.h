681cf7e0007e78ecdcc2b23bb41e33f5e8cd4160
huicongfu <347795319@qq.com>
2016-09-24 09:57:03 +0800

    define methods

--- FHCHUD/FHCHUD/FHCHUD/FHCHUD.h
+++ FHCHUD/FHCHUD/FHCHUD/FHCHUD.h
@@ -8,6 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger, FHCHUDMode){
+    /**
+     *  默认值是菊花动画模式
+     */
+    FHCHUDModeIndicator,
+    /**
+     *  只显示标题
+     */
+    FHCHUDModeText
+};
+
 @interface FHCHUD : UIView
+
+@property (nonatomic, assign) FHCHUDMode mode;
+@property (nonatomic, retain) NSString * labelText;
+
+- (instancetype)initWithView:(UIView *)view;
+- (void)show;
+- (void)hide;
 
 @end
--- FHCHUD/FHCHUD/FHCHUD/FHCHUD.m
+++ FHCHUD/FHCHUD/FHCHUD/FHCHUD.m
@@ -8,14 +8,65 @@
 
 #import "FHCHUD.h"
 
+#define MB_TEXTSIZE(text, font) [text length] > 0 ? [text \
+sizeWithAttributes:@{NSFontAttributeName:font}] : CGSizeZero;
+
+static CGFloat const kMargin = 20;
+static CGFloat const kPadding = 4;
+
+@interface FHCHUD ()
+
+@property (nonatomic, retain) UIView * indicatorView;
+@property (nonatomic, retain) UILabel * label;
+@property (nonatomic, assign) CGSize size;
+
+@end
+
 @implementation FHCHUD
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)dealloc{
+    
 }
-*/
+
+- (instancetype)initWithView:(UIView *)view{
+    return [self initWithFrame:view.bounds];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self == [super initWithFrame:frame]) {
+        _mode = FHCHUDModeIndicator;
+        _labelText = nil;
+        _size = CGSizeZero;
+        self.opaque = NO;
+        self.backgroundColor = [UIColor clearColor];
+        self.alpha = 0;
+        
+        [self setupView];
+        [self updateIndicators];
+        [self registerForKVO];
+    }
+    return self;
+}
+
+- (void)show{
+    self.alpha = 1;
+}
+
+- (void)hide{
+    self.alpha = 0;
+    [self removeFromSuperview];
+}
+
+- (void)setupView{
+    
+}
+
+- (void)updateIndicators{
+    
+}
+
+- (void)registerForKVO{
+    
+}
 
 @end
