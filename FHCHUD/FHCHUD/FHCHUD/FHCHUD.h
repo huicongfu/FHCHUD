d15dbe25a518afebd4cfd748b3f9efe193a0aa09
huicongfu <347795319@qq.com>
2016-09-24 09:56:19 +0800

    define methods

---
+++ FHCHUD/FHCHUD/FHCHUD/FHCHUD.h
@@ -0,0 +1,13 @@
+//
+//  FHCHUD.h
+//  FHCHUD
+//
+//  Created by hunuo on 16/9/24.
+//  Copyright © 2016年 fhc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FHCHUD : UIView
+
+@end
---
+++ FHCHUD/FHCHUD/FHCHUD/FHCHUD.m
@@ -0,0 +1,21 @@
+//
+//  FHCHUD.m
+//  FHCHUD
+//
+//  Created by hunuo on 16/9/24.
+//  Copyright © 2016年 fhc. All rights reserved.
+//
+
+#import "FHCHUD.h"
+
+@implementation FHCHUD
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
